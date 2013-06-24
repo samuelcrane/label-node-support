a2496c50b5bf403a2c277c6e6ea1a1b183dd570f
Samuel Crane <sam@samuelcrane.com>
2013-06-24 16:40:36 -0300

    Update labelNodeSupport.r

--- labelNodeSupport.r
+++ labelNodeSupport.r
@@ -63,9 +63,55 @@ plotBayesBoot <- function(bayesTree,bootTree)
     nodelabels(bootList, adj=c(1.4, 1.3), frame="n", cex=0.5, font=2) #Adds bootstrap values.
 }
 
+# Use plotBayesBootTwo to map three sets of suport values onto the target tree. This works the same
+# as plotBayesBoot but iterates across a third tree to compare with the first tree. The values are 
+# drawn in a different format as well. 
+plotBayesBootTwo <- function(bayesTree, bootTree1, bootTree2) 
+{
+    getAllSubtrees(bayesTree) -> bayesSub
+    getAllSubtrees(bootTree1) -> bootSub1
+    getAllSubtrees(bootTree2) -> bootSub2
+    bootList1 <- matrix("<50", Nnode(bayesTree), 1)
+    bootList2 <- matrix("<50", Nnode(bayesTree), 1) #Watch this <50 value if using a scrict consensus tree
+    
+    #The commands below compare all the subclades in the Bayes tree to all the subclades in the bootstrap tree, and vice versa, and identifies all those clades that are identical.
+    for(i in 1:Nnode(bayesTree)) 
+    {
+        for(j in 1:Nnode(bootTree1)) 
+        {
+            match(bayesSub[[i]]$tip.label[order(bayesSub[[i]]$tip.label)], bootSub1[[j]]$tip.label[order(bootSub1[[j]]$tip.label)]) -> shared
+            match(bootSub1[[j]]$tip.label[order(bootSub1[[j]]$tip.label)], bayesSub[[i]]$tip.label[order(bayesSub[[i]]$tip.label)]) -> shared2
+            if(sum(is.na(c(shared,shared2)))==0) 
+            {
+                bootTree1$node.label[j] -> bootList1[i]
+            }
+        }
+        for(j in 1:Nnode(bootTree2)) 
+        {
+            match(bayesSub[[i]]$tip.label[order(bayesSub[[i]]$tip.label)], bootSub2[[j]]$tip.label[order(bootSub2[[j]]$tip.label)]) -> shared
+            match(bootSub2[[j]]$tip.label[order(bootSub2[[j]]$tip.label)], bayesSub[[i]]$tip.label[order(bayesSub[[i]]$tip.label)]) -> shared2
+            if(sum(is.na(c(shared,shared2)))==0) 
+            {
+                bootTree2$node.label[j] -> bootList2[i]
+            }
+        }
+    }
+    plot(ladderize(bayesTree), cex=0.5, lwd=0.5, direction='r', use.edge.length=FALSE, label.offset=1, x.lim=c(0.0000, 300), y.lim=c(0, 110)) #Plots your Bayesian consensus tree
+    nodelabels(bayesTree$node.label, adj=c(-0.1, -0.1), frame="n", cex=0.5, font=2) #Adds posterior probability values to the tree. Change the 'cex' value to make the fond smaller or larger. A value of 1 will give you a readable result in the R quartz window, but a value closer to 0.25 might be better for publication)
+    nodelabels(bootList1, adj=c(1.2, -0.5), frame="n", cex=0.5, font=1) #Adds bootstrap values.
+    nodelabels(bootList2, adj=c(1.2, 1.5), frame="n", cex=0.5, font=3) #Adds bootstrap values.
+}
 
-#Now you're ready to read your trees in and make your figure! [RG]
+
+# Read in two trees
 read.nexus("yourBayesTree.con")->bayesTree #Reads in the .con file that results from analyses in MrBayes.
 bayesTree[[1]]->bayesTree #Extracts one of the two trees in the .con file.
 read.nexus("yourBootTree.nex")->bootTree #Reads in the consensus tree from a bootstrap analysis in PAUP.
-plotBayesBoot(bayesTree, bootTree)
+plotBayesBoot(bayesTree, bootTree) # For two trees
+
+# Read in three trees
+read.nexus("yourBayesTree.con")->bayesTree #Reads in the .con file that results from analyses in MrBayes.
+bayesTree[[1]]->bayesTree #Extracts one of the two trees in the .con file.
+read.nexus("yourBootTree.nex")->bootTree1
+read.nexus("yourOtherBootTree.nex")->bootTree2
+plotBayesBootTwo(bayesTree, bootTree1, bootTree2) # For three trees (e.g. Bayesian, ML, and MP)
